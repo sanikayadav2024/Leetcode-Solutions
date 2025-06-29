d305d853d897d524db3699422649bf75a92614a4
sanika yadav <codegirlsanika@gmail.com>
2025-06-29 21:32:53 +0530

    Time: 1 ms (98.31%) | Memory: 45 MB (30.85%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,16 +1,29 @@
 class Solution {
-    public void duplicateZeros(int[] arr) {
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                shift(arr , i);
-                i++;
-            }
-        }
+      /*
+  O(n) runtime
+  O(1) space
+  */
+  public void duplicateZeros(int[] A) {
+    
+    int n = A.length, count = 0;
+    
+    for (int num : A) if (num == 0) count++;
+    int i = n - 1;
+    int write = n + count - 1;
+    
+    while (i >= 0 && write >= 0)  {
+      
+      if (A[i] != 0) { // Non-zero, just write it in
+        if (write < n) A[write] = A[i];
+        
+      } else { // Zero found, write it in twice if we can
+        if (write < n) A[write] = A[i];
+        write--;
+        if (write < n) A[write] = A[i];
+      }
+      
+      i--;
+      write--;
     }
-
-    void shift(int[] arr, int i){
-        for(int j = arr.length - 1; j > i; j--){
-            arr[j] = arr[j-1];
-        }
-    }
+  }
 }
