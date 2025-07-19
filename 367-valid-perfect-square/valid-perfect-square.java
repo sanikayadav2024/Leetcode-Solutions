1f02b96debf369adebe1137dd6e5d4542b6d2a88
sanika yadav <codegirlsanika@gmail.com>
2025-07-19 21:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (51.47%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 0;
+        long end = num;
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+            if( mid* mid == num) return true;
+            
+            if(mid * mid < num) start = mid + 1;
+            else end = mid - 1;
+        }
+        return false;
+    }
+}
