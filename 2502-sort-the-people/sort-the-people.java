f932ffb7269be00e301f46e9d1669da2071c329c
sanika yadav <codegirlsanika@gmail.com>
2025-09-10 18:59:46 +0530

    Time: 7 ms (92.50%) | Memory: 45.6 MB (42.23%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,20 +1,34 @@
 class Solution {
     public String[] sortPeople(String[] n, int[] h) {
+        // for(int i = 0; i < h.length; i++){
+        //     int max = i;
+        //     for(int j = i + 1; j < h.length; j++){
+        //         if(h[max] < h[j]){
+        //             max = j;
+        //         }
+        //     }
+        //     if(max != i){
+        //         int temp = h[i];
+        //         String name = n[i];
+        //         h[i] = h[max];
+        //         n[i] = n[max];
+        //         h[max] = temp;
+        //         n[max] = name;
+        //     }
+        // }
+        // return n;
+
+        HashMap<Integer, String> map = new HashMap<>();
+
         for(int i = 0; i < h.length; i++){
-            int max = i;
-            for(int j = i + 1; j < h.length; j++){
-                if(h[max] < h[j]){
-                    max = j;
-                }
-            }
-            if(max != i){
-                int temp = h[i];
-                String name = n[i];
-                h[i] = h[max];
-                n[i] = n[max];
-                h[max] = temp;
-                n[max] = name;
-            }
+            map.put(h[i], n[i]);
+        }
+
+        Arrays.sort(h);
+        int j = 0;
+
+        for(int i = h.length - 1; i >= 0; i--){
+            n[j++] = map.get(h[i]);
         }
         return n;
     }
