ede7439f43eb827e299799fbaa2bf5f0133ddfb1
sanika yadav <codegirlsanika@gmail.com>
2025-09-20 19:15:30 +0530

    Time: 2 ms (99.91%) | Memory: 43.3 MB (71.91%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -1,15 +1,25 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        for(List<String> p : paths){
-            String temp = p.get(1);
-            boolean found = false;
-            for(List<String> s : paths){
-               if(temp.equals(s.get(0))){
-                found = true;
-               }
-            }
-            if(!found){
-                return temp;
+        // for(List<String> p : paths){
+        //     String temp = p.get(1);
+        //     boolean found = false;
+        //     for(List<String> s : paths){
+        //        if(temp.equals(s.get(0))){
+        //         found = true;
+        //        }
+        //     }
+        //     if(!found){
+        //         return temp;
+        //     }
+        // }
+
+        Set<String> set = new HashSet<>();
+        for(List<String> source: paths){
+            set.add(source.get(0));
+        }
+        for(List<String> dest : paths){
+            if(!set.contains(dest.get(1))){
+                return dest.get(1);
             }
         }
         return " ";
