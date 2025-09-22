5bde76b15117e0f4b296d43348b6fa7f5fc4839e
sanika yadav <codegirlsanika@gmail.com>
2025-09-22 20:50:13 +0530

    Time: 2 ms (75.64%) | Memory: 42.3 MB (73.51%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,17 +1,30 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for(int num : nums){
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
-        }
-        int max = Collections.max(freq.values());
-        int count = 0;
+        // Map<Integer, Integer> freq = new HashMap<>();
+        // for(int num : nums){
+        //     freq.put(num, freq.getOrDefault(num, 0) + 1);
+        // }
+        // int max = Collections.max(freq.values());
+        // int count = 0;
 
-        for(int c : freq.values()){
-            if(c == max){
-                count += c;
+        // for(int c : freq.values()){
+        //     if(c == max){
+        //         count += c;
+        //     }
+        // }
+        // return count;
+
+        int[] freq = new int[101];
+        for(int num : nums){
+            freq[num] += 1;
+        }
+        Arrays.sort(freq);
+        int cnt = 0;
+        for(int i = freq.length - 1; i >= 0; i--){
+            if(freq[i] == freq[freq.length - 1]){
+                cnt += freq[i];
             }
         }
-        return count;
+        return cnt;
     }
 }
