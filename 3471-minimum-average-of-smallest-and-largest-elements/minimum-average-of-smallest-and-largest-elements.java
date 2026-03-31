92bd39cc0fc7efa6d1cb7a42ca651906a5b15576
sanika yadav <codegirlsanika@gmail.com>
2026-03-31 15:44:39 +0530

    Time: 5 ms (91.34%) | Memory: 48.5 MB (15.28%) - LeetSync

--- 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
+++ 3471-minimum-average-of-smallest-and-largest-elements/minimum-average-of-smallest-and-largest-elements.java
@@ -2,11 +2,10 @@ class Solution {
     public double minimumAverage(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        double[] ans = new double[n/2];
+        double ans = Integer.MAX_VALUE;
         for(int i = 0; i < n/2; i++){
-            ans[i] = (double)(nums[i] + nums[n-i-1])/2;
+            ans = Math.min((double)(nums[i] + nums[n-i-1])/2 , ans);
         }
-        Arrays.sort(ans);
-        return ans[0];
+        return ans;
     }
 }
