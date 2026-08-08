133b3d6026906cd3c936dbc09c0c42ecb0d00bb5
sanika yadav <codegirlsanika@gmail.com>
2026-08-08 13:06:11 +0530

    Time: 2131 ms (5.73%) | Memory: 42.4 MB (10.29%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) 
+    {
+        int sum=0;
+        int i;
+        for(i=1;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                sum=sum+i;
+            }
+        }
+        if(num==sum)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
