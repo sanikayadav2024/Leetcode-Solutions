a37b2400063cfad647c74d5eb7d43dc5a5c703fa
sanika yadav <codegirlsanika@gmail.com>
2026-08-28 19:06:15 +0530

    Time: 5 ms (83.11%) | Memory: 77.2 MB (69.03%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -2,13 +2,15 @@ class Solution {
     public int maxArea(int[] h) {
         int l = 0;
         int u = h.length-1;
-        int maxVol = Integer.MIN_VALUE;
+
+        int max = 0;
+
         while(l < u){
-            int vol = Math.min(h[l], h[u])*(u-l);
-            maxVol = Math.max(maxVol, vol);
-            if(h[u] < h[l]) u--;
-            else l++;
+            int curr = Math.min(h[l], h[u]) * (u-l);
+            max = Math.max(curr, max);
+            if(h[l] < h[u]) l++;
+            else u--;
         }
-        return maxVol;
+        return max;
     }
 }
